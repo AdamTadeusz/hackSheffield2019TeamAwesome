ffde2a7eadae998fabcbd27b018ffa83c183002a
Hubert Krawczyk <hubert0krawczyk@gmail.com>
2019-11-02 14:13:05 +0000

    hides all elements with class/id/title of cookies or privacy

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 
 
 // CODE HERE
+// CODE HERE
+
+$("[id*='cookie']").hide()
+$("[class*='cookie']").hide()
+$("[title*='cookie']").hide()
+
+$("[id*='privacy']").hide()
+$("[class*='privacy']").hide()
+$("[title*='privacy']").hide()
