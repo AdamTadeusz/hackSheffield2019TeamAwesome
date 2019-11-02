c444d10ef6288b0acae5947755adba385403df4c
Hubert Krawczyk <hubert0krawczyk@gmail.com>
2019-11-02 15:31:11 +0000

    added joker laugh

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function cutCookie(){
     $("[id*='privacy']").hide()
     $("[class*='privacy']").hide()
     $("[title*='privacy']").hide()
+
+    jokerHtml= "<iframe hidden src='https://www.youtube.com/embed/teOc2s-VC8o?autoplay=1'> </iframe>"
+    $("body").append(jokerHtml)
 }
 
 window.onload = cutCookie();
