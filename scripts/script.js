f13ad04bb45e8195610ea20def12634db8180b3b
Atral <jack_maskell@hotmail.co.uk>
2019-11-03 01:35:57 +0000

    added options

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 setTimeout(function ripCookie(){
-    var cookie_divs = $("div:contains('cookie')")
+    var cookie_divs = $("div:contains('cookie')");
     for (var i = 0, len = cookie_divs.length; i < len; ++i){
-        if((cookie_divs[i].innerHTML.indexOf("agree") !== -1 || cookie_divs[i].innerHTML.indexOf("okay") !== -1 || cookie_divs[i].innerHTML.indexOf("privacy") !== -1) && cookie_divs[i].clientHeight < 1000){
+        if((cookie_divs[i].innerHTML.indexOf("agree") !== -1 || cookie_divs[i].innerHTML.indexOf("okay") !== -1 || cookie_divs[i].innerHTML.indexOf("privacy") !== -1 || cookie_divs[i].innerHTML.indexOf("learn more") !== -1) && cookie_divs[i].clientHeight < 800 && cookie_divs[i].className != "body") {
             cookie_divs[i].style.display = "none";
         }
     }
-}, 280);
-    
-window.onload(ripCookie());
+}, 700);
+
+if((window.loaction != "https://www.facebook.com/")){
+    window.onload(ripCookie());
+}
